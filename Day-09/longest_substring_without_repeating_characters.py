40f3937974fa9e365d2d71c14ac3cd7889783e94
Shivani Shah <shivanishahwork2@gmail.com>
2024-05-26 13:54:28 -0700

    Fixing some comments

--- Day-09/longest_substring_without_repeating_characters.py
+++ Day-09/longest_substring_without_repeating_characters.py
@@ -31,7 +31,10 @@ class Solution:
         maxLength = 0 # Variable to maintain maximum length of the substring seen so far
         d = {} # Dictionary to maintain letters as key and the index of the letter seen latest (when repeated) as the value
         while end < len(s): # Run the loop until the end pointer reaches the last index
-            if s[end] in d and d[s[end]] >= start: # If the value at the end pointer in `s` exist in the dictionary and if the value for that letter in dictionary is greater than the value of the start pointer i.e. if the index of that letter in the dictionary is greater than the index at which the start pointer is pointing (that means that the letter is repeated!)
+            # If the value at the end pointer in `s` exist in the dictionary and if the value for that letter in dictionary is greater than the 
+            # value of the start pointer i.e. if the index of that letter in the dictionary is greater than the index at which the start pointer is pointing 
+            # (that means that the letter is repeated!)
+            if s[end] in d and d[s[end]] >= start: 
                 start = d[s[end]] + 1 # Update the value of the start pointer to the index value of index next to the value for the letter in the dictionary
             maxLength = max(maxLength, end - start + 1) # Calculate the maxLength. end - start + 1 because we need to include end and start both
             d[s[end]] = end # Update the value for the letter in the dictionary to the latest index where it was last seen
